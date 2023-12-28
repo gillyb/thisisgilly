56f72f1758e9ed3da682c213d7af863a4acd556e
Gilly Barr <gillyb@gmail.com>
2023-12-28 18:56:03 +0200

    Fast commit baby

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -24,6 +25,7 @@ export default function RootLayout({
         <link rel="manifest" href="/site.webmanifest"/>
       </head>
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
