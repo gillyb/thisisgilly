bc0915580f5573654d9613ea7d17dd8d5c667f31
Gilly Barr <gillyb@gmail.com>
2024-01-21 09:02:15 +0200

    Fast commit baby

--- src/app/data/experience.ts
+++ src/app/data/experience.ts
@@ -1,37 +1,64 @@
-export const experience = [{
-  name: 'Palo Alto Networks',
-  title: 'Principal Engineer',
-  dates: 'August 2021 - Current',
-  imageUrl: '/images/paloalto_logo.jpeg',
-  description: ['Working on core features of Cortex XDR, one of Palo Alto\'s fastest growing products. Owner of the XQL editor tool inside the app - XQL is a proprietary language for querying user data. I worked on adding many new features to the language, defining the grammar and implementing live error checking and coding suggestions while typing.', 'Worked with Angular & React. Used the ANTLR library for defining the XQL grammar and creating listeners for error checking and suggestions.']
-}, {
-  name: 'Google',
-  title: 'Senior Software Engineer (Level 4)',
-  dates: 'February 2018 - July 2021',
-  imageUrl: '/images/google_logo.jpeg',
-  description: ['On the team that supported the Trends tool and also built Pinpoint - a tool for investigative journalists to work with large amounts of documents.', 'Worked mainly with Java and Typescript, along with many internal proprietary frameworks and tools.']
-}, {
-  name: 'Logz.io',
-  title: 'Full Stack Team Leader',
-  dates: 'December 2014 - December 2017',
-  imageUrl: '/images/logzio_logo.jpeg',
-  description: ['First employee of the company - Built the initial system from scratch along with one other developer (and slowly grew). After ~a year became the Front End team leader - hired 7 employees that directly reported to me.', 'Worked extensively on the kibana source code, making many modifications for our use.', 'Worked extensively with AngularJS & React. Used a little java & nodejs. Became very familiar with ElasticSearch & kafka.']
-}, {
-  name: 'Sears Israel',
-  title: 'Full Stack Developer',
-  dates: 'October 2011 - May 2014',
-  imageUrl: '/images/sears_logo.jpeg',
-  description: ['Full stack developer on the ShopYourWay.com website - Implemented many new features end to end. After ~a year transitioned to the infrastructure team where I worked on core features like implementing a new in-house queue management system and being responsible for the frontend performance. Gave talks within the company and implemented a performance-first approach.', 'Wrote a lot of code with C# and Java.', 'Used various storage systems - ElasticSearch, redis, MySQL, Gigaspaces']
-}, {
-  name: 'Visa CAL (ICC)',
-  title: '.NET Developer',
-  dates: 'August 2009 - November 2011',
-  imageUrl: '/images/cal_logo.jpeg',
-  description: ['Worked on the customer facing website which users can view their credit card transactions and a personal financial management tool. I was the sole owner of the internal tool that the sales team used to register and manage new user\'s credit cards.', 'Worked mostly with C# on the backend, and used asp.net along with jquery on the frontend.']
-}, {
-  name: 'Israeli Air Force',
-  title: 'Software Engineer',
-  dates: 'February 2006 - August 2009',
-  imageUrl: '/images/idf_logo.jpeg',
-  description: ['Built and maintained internal applications/tools.', 'Worked with C#, asp.net, memcached, Oracle DB']
-}];
+export const experience = [
+  {
+    name: "Palo Alto Networks",
+    title: "Principal Engineer",
+    dates: "August 2021 - Current",
+    imageUrl: "/images/paloalto_logo.jpeg",
+    description: [
+      "Working on core features of Cortex XDR, one of Palo Alto's fastest growing products. Owner of the XQL editor tool inside the app - XQL is a proprietary language for querying user data. I worked on adding many new features to the language, defining the grammar and implementing live error checking and coding suggestions while typing.",
+      "Worked with Angular & React. Used the ANTLR library for defining the XQL grammar and creating listeners for error checking and suggestions.",
+    ],
+  },
+  {
+    name: "Google",
+    title: "Senior Software Engineer (Level 4)",
+    dates: "February 2018 - July 2021",
+    imageUrl: "/images/google_logo.jpeg",
+    description: [
+      'On the team that maintained the <a href="https://trends.google.com/trends/" target="_blank">Trends</a> tool and also built <a href="https://journaliststudio.google.com/pinpoint/about" target="_blank">Pinpoint</a> - a tool for investigative journalists to work with large amounts of documents.',
+      "Worked mainly with Java and Typescript, along with many internal proprietary frameworks and tools.",
+    ],
+  },
+  {
+    name: "Logz.io",
+    title: "Full Stack Team Leader",
+    dates: "December 2014 - December 2017",
+    imageUrl: "/images/logzio_logo.jpeg",
+    description: [
+      "First employee of the company - Built the initial system from scratch along with one other developer (and slowly grew). After ~a year became the Front End team leader - hired and managed 7 employees that directly reported to me.",
+      "Worked extensively on the kibana source code, making many modifications for our use.",
+      "Worked extensively with AngularJS & React. Used a little java & nodejs. Became very familiar with ElasticSearch & kafka.",
+    ],
+  },
+  {
+    name: "Sears Israel",
+    title: "Full Stack Developer",
+    dates: "October 2011 - May 2014",
+    imageUrl: "/images/sears_logo.jpeg",
+    description: [
+      "Full stack developer on the ShopYourWay.com website - Implemented many new features end to end. After ~a year transitioned to the infrastructure team where I worked on core features like implementing a new in-house queue management system and being responsible for the frontend performance. Gave talks within the company and implemented a performance-first approach.",
+      "Wrote a lot of code with C# and Java.",
+      "Used various storage systems - ElasticSearch, redis, MySQL, Gigaspaces",
+    ],
+  },
+  {
+    name: "Visa CAL (ICC)",
+    title: ".NET Developer",
+    dates: "August 2009 - November 2011",
+    imageUrl: "/images/cal_logo.jpeg",
+    description: [
+      "Worked on the customer facing website which users can view their credit card transactions and a personal financial management tool. I was the sole owner of the internal tool that the sales team used to register and manage new user's credit cards.",
+      "Worked mostly with C# on the backend, and used asp.net along with jquery on the frontend.",
+    ],
+  },
+  {
+    name: "Israeli Air Force",
+    title: "Software Engineer",
+    dates: "February 2006 - August 2009",
+    imageUrl: "/images/idf_logo.jpeg",
+    description: [
+      "Built and maintained internal applications/tools.",
+      "Worked with C#, asp.net, memcached, Oracle DB",
+    ],
+  },
+];
